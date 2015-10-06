b39b80d30d39710dca6d7de593627e1a90231631
Oliastic <mail@oliastic.de>
2015-10-07 00:11:45 +0200

    Update default parameters

--- assets/js/config.js
+++ assets/js/config.js
@@ -21,8 +21,8 @@ var my_theme_do_display_search_field_in_separate_row  = false;
 //**********************
 var my_theme_bonus_page_name_1				= 'About';
 var my_theme_bonus_page_link_1				= '/about';
-var my_theme_bonus_page_name_2				= 'Links';
-var my_theme_bonus_page_link_2				= '/links';
+var my_theme_bonus_page_name_2				= '';
+var my_theme_bonus_page_link_2				= '';
 var my_theme_bonus_page_name_3				= '';
 var my_theme_bonus_page_link_3				= '';
 
@@ -33,12 +33,12 @@ var my_theme_bonus_page_link_3				= '';
 var my_theme_static_page_do_show_time_stamp 		= false;
 var my_theme_static_page_do_show_share_links 		= false;
 var my_theme_static_page_do_show_author_info 		= false;
-var my_theme_static_page_do_show_comments 			= true;
+var my_theme_static_page_do_show_comments 			= false;
 
 
 //**********************
 //	DISQUS THREAD
 //**********************
-var my_theme_disqus_do_activate_comments				= true;
-var my_theme_disqus_do_create_thread_per_post 	= true;
-var my_theme_disqus_shortname 								  = 'oliastictest2';
+var my_theme_disqus_do_activate_comments			 = false;
+var my_theme_disqus_do_create_thread_per_post  = true;
+var my_theme_disqus_shortname 						     = '';
